46f86084f18f2afb1fdf02851c416bdb5b9945a6
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-06-19 11:52:48 +0530

    learn terraform

--- 02-ec2/ec2instance.tf
+++ 02-ec2/ec2instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "main" {
   }
 }
 
-resource "aws_instance" "main" {
+resource "aws_instance" "testserve2" {
   ami           = "ami-010baf2054632536e"
   instance_type = "t3.micro"
 
